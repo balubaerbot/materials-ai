05759fea404b27cf9178bb3ace499d61ee6187d8
balubaerbot <robert.schnablehner@gmail.com>
2026-09-20 14:13:29 +0200

    aktivierungsfunktion auf .TanH()

--- modules/02_mlp_activation/model.py
+++ modules/02_mlp_activation/model.py
@@ -27,7 +27,8 @@ class MLP(nn.Module):
         layers: list[nn.Module] = [nn.Linear(1, hidden_size), nn.TanH()]
         for _ in range(n_hidden_layers - 1):
             layers.append(nn.Linear(hidden_size, hidden_size))
-            layers.append(nn.ReLU())
+            """layers.append(nn.ReLU())"""
+            layers.append(nn.TanH())
         layers.append(nn.Linear(hidden_size, 1))
 
         self.net = nn.Sequential(*layers)
